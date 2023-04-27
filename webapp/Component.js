ade49c02ad2e7ef2dbf368a9f364aa1ea8d9a81a
Rafael Fortes <rafael.lacerda@seidor.com.br>
2023-04-27 10:50:56 +0000

    criacao do projeto

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("br.com.seidor.crud.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
